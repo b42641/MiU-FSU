9f1463b014a5405916528e404c93745fb3a12b70
David Peterson <dbpete@yahoo.com>
2013-07-21 13:33:39 -0400

    Resubmission with gold and bronze files in separate directories

---
+++ Peterson_David_project2/goldApp/js/json.js
@@ -0,0 +1,182 @@
+var json = {
+     // 1 seed book
+	"record1":{
+	"itemName"     : ["Item Name", "Making of Star Wars Video"],
+    "mediaType"    : ["Media Type", "movie"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Amazon"],
+    "priority"     : ["Priority", "99"],
+    "desireDate"     : ["DesiredDate", "01/11/2011"],
+    "description"    : ["Description", "How the movie was made, etc."]
+	},
+    // 2 seed book
+    "record2":{
+    "itemName"     : ["Item Name", "Inferno - Dan Brown"],
+    "mediaType"    : ["Media Type", "book"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Amazon"],
+    "priority"     : ["Priority", "22"],
+    "desireDate"     : ["DesiredDate", "02/12/2012"],
+    "description"    : ["Description", "Dan's latest book"]
+	},
+    // 3 seed book
+    "record3":{
+    "itemName"     : ["Item Name", "Doc"],
+    "mediaType"    : ["Media Type", "audible"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Audible"],
+    "priority"     : ["Priority", "33"],
+    "desireDate"     : ["DesiredDate", "03/13/2013"],
+    "description"    : ["Description", "Saddest sports story ever"]
+	},
+    // 4 seed book
+    "record4":{
+    "itemName"     : ["Item Name", "ColdPlay"],
+    "mediaType"    : ["Media Type", "music"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "iTunes"],
+    "priority"     : ["Priority", "44"],
+    "desireDate"     : ["DesiredDate", "04/14/2014"],
+    "description"    : ["Description", "Not sure which album to listen to"]
+	},
+    // 5 seed book
+    "record5":{
+    "itemName"     : ["Item Name", "Kojak Seaons One"],
+    "mediaType"    : ["Media Type", "movie"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Amazon"],
+    "priority"     : ["Priority", "49"],
+    "desireDate"     : ["DesiredDate", "06/11/2013"],
+    "description"    : ["Description", "Best season is one"]
+	},
+    // 6 seed book
+    "record6":{
+    "itemName"     : ["Item Name", "Enders game Orson Scott Card"],
+    "mediaType"    : ["Media Type", "book"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Amazon"],
+    "priority"     : ["Priority", "75"],
+    "desireDate"     : ["DesiredDate", "12/12/2013"],
+    "description"    : ["Description", "The book that started it all"]
+	},
+    // 7 seed book
+    "record7":{
+    "itemName"     : ["Item Name", "Black Swan"],
+    "mediaType"    : ["Media Type", "audible"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Audible"],
+    "priority"     : ["Priority", "33"],
+    "desireDate"     : ["DesiredDate", "12/25/2013"],
+    "description"    : ["Description", "Recommended Business book"]
+	},
+    // 8 seed book
+    "record8":{
+    "itemName"     : ["Item Name", "Bob Dylan Dylan"],
+    "mediaType"    : ["Media Type", "music"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "iTunes"],
+    "priority"     : ["Priority", "44"],
+    "desireDate"     : ["DesiredDate", "01/14/2014"],
+    "description"    : ["Description", "Dylans First Album"]
+	},
+    // 9 seed book
+    "record9":{
+    "itemName"     : ["Item Name", "Jeremiah Johnson"],
+    "mediaType"    : ["Media Type", "movie"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Amazon"],
+    "priority"     : ["Priority", "88"],
+    "desireDate"     : ["DesiredDate", "01/11/2014"],
+    "description"    : ["Description", "Favorite Redford Movie"]
+	},
+    // 10 seed book
+    "record10":{
+    "itemName"     : ["Item Name", "Adams McCullough"],
+    "mediaType"    : ["Media Type", "book"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Amazon"],
+    "priority"     : ["Priority", "22"],
+    "desireDate"     : ["DesiredDate", "07/01/2012"],
+    "description"    : ["Description", "Greatest American"]
+	},
+    // 11 seed book
+    "record11":{
+    "itemName"     : ["Item Name", "Cod"],
+    "mediaType"    : ["Media Type", "audible"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Audible"],
+    "priority"     : ["Priority", "33"],
+    "desireDate"     : ["DesiredDate", "12/25/2013"],
+    "description"    : ["Description", "Strange fish Book"]
+	},
+    // 12 seed book
+    "record12":{
+    "itemName"     : ["Item Name", "Slice Five for Fighting"],
+    "mediaType"    : ["Media Type", "music"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "iTunes"],
+    "priority"     : ["Priority", "88"],
+    "desireDate"     : ["DesiredDate", "12/14/2014"],
+    "description"    : ["Description", "Last Albi,"]
+	},
+    // 13 seed book
+    "record13":{
+    "itemName"     : ["Item Name", "Sopranos"],
+    "mediaType"    : ["Media Type", "movie"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Amazon"],
+    "priority"     : ["Priority", "99"],
+    "desireDate"     : ["DesiredDate", "12/12/2014"],
+    "description"    : ["Description", "The most watched TV series"]
+	},
+    // 14 seed book
+    "record14":{
+    "itemName"     : ["Item Name", "Suduku"],
+    "mediaType"    : ["Media Type", "book"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Amazon"],
+    "priority"     : ["Priority", "99"],
+    "desireDate"     : ["DesiredDate", "02/12/2013"],
+    "description"    : ["Description", "Brain Exercise"]
+	},
+    // 15 seed book
+    "record15":{
+    "itemName"     : ["Item Name", "Woodrow Wilson"],
+    "mediaType"    : ["Media Type", "audible"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Audible"],
+    "priority"     : ["Priority", "66"],
+    "desireDate"     : ["DesiredDate", "12/13/2013"],
+    "description"    : ["Description", "Not sure of the author's name"]
+	},
+    // 16 seed book
+    "record16":{
+    "itemName"     : ["Item Name", "Creedence Clearwater"],
+    "mediaType"    : ["Media Type", "music"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "iTunes"],
+    "priority"     : ["Priority", "66"],
+    "desireDate"     : ["DesiredDate", "12/12/2014"],
+    "description"    : ["Description", "Any Album"]
+	},
+    // 17 seed book
+    "record17":{
+    "itemName"     : ["Item Name", "1984"],
+    "mediaType"    : ["Media Type", "movie"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Amazon"],
+    "priority"     : ["Priority", "66"],
+    "desireDate"     : ["DesiredDate", "01/11/2013"],
+    "description"    : ["Description", "The Old One"]
+	},
+    // 18 seed book
+    "record18":{
+    "itemName"     : ["Item Name", "IOS 8 Programming"],
+    "mediaType"    : ["Media Type", "book"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Amazon"],
+    "priority"     : ["Priority", "99"],
+    "desireDate"     : ["DesiredDate", "09/12/2013"],
+    "description"    : ["Description", "Pick Author when books come out"]
+	},
+    // 19 seed book
+    "record19":{
+	"itemName"     : ["Item Name", "D-Day Ambrose"],
+    "mediaType"    : ["Media Type", "audible"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "Audible"],
+    "priority"     : ["Priority", "33"],
+    "desireDate"     : ["DesiredDate", "12/13/2013"],
+    "description"    : ["Description", "Gift for my Dad"]
+	},
+    // 20 seed book
+    "record20":{
+    "itemName"     : ["Item Name", "Nickel Creek When In Rome"],
+	"mediaType"    : ["Media Type", "music"],   //eLement('mediaType').value],
+    "desiredVendor"  : ["Desired Vendor", "iTunes"],
+    "priority"     : ["Priority", "55"],
+    "desireDate"     : ["DesiredDate", "05/15/2014"],
+	"description"    : ["Description", "Neo BLuegrass"]
+	}
+}
